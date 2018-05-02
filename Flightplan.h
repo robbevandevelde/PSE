863afebcf25ce48221438d08488ccc465b10e9de
Robbeheid <vandevelde.robbe@gmail.com>
2018-05-02 20:08:36 +0200

    flightplan pre and post conditions

--- Flightplan.h
+++ Flightplan.h
@@ -4,6 +4,7 @@
 
 #ifndef PSE_FLIGHTPLAN_H
 #define PSE_FLIGHTPLAN_H
+#include "DesignByContract.h"
 
 #include <string>
 using namespace std;
@@ -14,18 +15,21 @@ private:
     unsigned int departure;
     unsigned int arrival;
     unsigned int interval;
+    Flightplan* initCheck;
 
 public:
 
-    void setDestination(const string &destination);
+    bool properlyInitialised();
+
+    void setDestination(string &destination);
     void setDeparture(unsigned int departure);
     void setArrival(unsigned int arrival);
     void setInterval(unsigned int interval);
 
-    unsigned int getArrival() const;
-    unsigned int getInterval() const;
-    unsigned int getDeparture() const;
-    const string &getDestination() const;
+    unsigned int getArrival();
+    unsigned int getInterval();
+    unsigned int getDeparture();
+    string &getDestination();
 
 
     Flightplan(std::string& dest, unsigned int dep, unsigned int arr, unsigned int inter);
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/uauser/Downloads/PSE
+# Install script for directory: /Users/robbe/Desktop/Projecten_semester_2/PSE/PSE
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/uauser/Downloads/PSE/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/robbe/Desktop/Projecten_semester_2/PSE/PSE/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
