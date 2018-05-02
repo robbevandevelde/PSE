ff4dcb9089ee493fd49fb1e8640b399a574540b6
Robbeheid <vandevelde.robbe@gmail.com>
2018-05-02 23:58:20 +0200

    Flightplan header comments added

--- Flightplan.h
+++ Flightplan.h
@@ -18,20 +18,55 @@ private:
     Flightplan* initCheck;
 
 public:
-
+    /* Contract
+    * PRE:
+    * POST:
+    */
     bool properlyInitialised();
-
+    /* Contract
+    * PRE:
+    * POST:
+    */
     void setDestination(string &destination);
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST:destination is now the given parameter
+    */
     void setDeparture(unsigned int departure);
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST:departure is now the given parameter
+    */
     void setArrival(unsigned int arrival);
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST:arrival is now the given parameter
+    */
     void setInterval(unsigned int interval);
-
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST:interval is now the given parameter
+    */
     unsigned int getArrival();
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST:returned arrival
+    */
     unsigned int getInterval();
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST::returned arrival
+    */
     unsigned int getDeparture();
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST::returned arrival
+    */
     string &getDestination();
-
-
+    /* Contract
+    * PRE:Must be Properly initialised
+    * POST::returned arrival
+    */
     Flightplan(std::string& dest, unsigned int dep, unsigned int arr, unsigned int inter);
 
 
